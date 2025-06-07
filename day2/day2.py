6b218fa43a398879d4a5f0c3ecfd2062ec688b9d
Antoine GROLLEAU <ant.grolleau@gmail.com>
2025-06-07 23:36:25 +0200

    Finalisation du day 2

--- day2/day2.py
+++ day2/day2.py
@@ -14,13 +14,37 @@ for groupe in groupes:
 
 # Partie 1
 
+def is_safe(rapport):
+    return (
+        (
+            all(rapport[i+1] > rapport[i] for i in range(len(rapport) - 1)) or
+            all(rapport[i+1] < rapport[i] for i in range(len(rapport) - 1))
+        )
+        and all((1 <= abs(rapport[i+1] - rapport[i]) <= 3) for i in range(len(rapport) - 1))
+    )
+
+
 nbr_de_safe = sum(
     1 for rapport in groupes
     if (
-        all(rapport[i+1] > rapport[i] for i in range(len(rapport)-1)) or
-        all(rapport[i+1] < rapport[i] for i in range(len(rapport)-1))
+        is_safe(rapport)
     )
-    and all(1 <= abs(rapport[i+1] - rapport[i]) <= 3 for i in range(len(rapport)-1))
 )
 
 print ("le nombre de raport safe est de ",nbr_de_safe)
+
+# Partie 2
+
+def is_safe_suppr(rapport):
+    if is_safe(rapport):
+        return True
+
+    for i in range(len(rapport)):
+        copie = rapport[:i] + rapport[i+1:]  # crée une nouvelle liste sans l’élément à l’index i
+        if is_safe(copie):
+            return True
+
+    return False
+
+nbr_real_safe = sum(1 for rapport in groupes if is_safe_suppr(rapport))
+print("Le nombre de rapports safe en prenant compte le problème est :", nbr_real_safe)
